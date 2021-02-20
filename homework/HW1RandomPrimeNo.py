1456e76fc4152c583f350a5556678ccd922313ac
bozarin <71145791+bozarin@users.noreply.github.com>
2021-02-21 00:53:41 +0200

    Create HW1RandomPrimeNo.py

---
+++ homework/HW1RandomPrimeNo.py
@@ -0,0 +1,30 @@
+#PRIME NUMBER QUESTION
+
+import random
+def randomprime(x,y):  
+    
+    x=1000
+    
+    while x==1000:
+        
+    control = 0
+        
+    c = int(random.randint(x,y))
+        
+        if c!=1:
+            for i in range(2,j):
+                if ( j%i==0 ):  
+                    control=control+1  
+
+            if (control!=0):
+            p=1000
+        
+        else:
+        return c
+        break
+        else:
+            p=1000
+
+for w in range(3): 
+  for v in range(3):
+        print(randomprime(1,1000), end=" ")    
