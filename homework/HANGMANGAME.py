718c02693e1647be8eb3eacae0f94cbd44628da8
bozarin <71145791+bozarin@users.noreply.github.com>
2021-02-21 22:09:42 +0200

    Update HANGMANGAME.py

--- homework/HANGMANGAME.py
+++ homework/HANGMANGAME.py
@@ -2,9 +2,29 @@ import random
 import sys
 
 
+
+
+
 def main():
 name=input("Please enter your name: ")
 print("Welcome {name}, this is the Hangman game!")
 
 keyword=input("Please enter a word: ")
 print("_ "*(len(keyword)))
+
+
+def display(key, guessed_words):
+
+        word_displayed = "  "
+
+     for letter in keyword:
+            if letter in guessed_words:
+                word_displayed += " " + letter + " "
+
+     else:
+      word_displayed += " _____ "
+
+        print "This is your word so far:", word_displayed, 
+        print("\n")
+
+        return word_displayed
